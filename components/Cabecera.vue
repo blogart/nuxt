f71b0ebd4f994c80f1c3f419c3fe52b7baa465b4
blogart <blogart89@gmail.com>
2021-07-26 13:14:10 +0200

    pinta-apis

---
+++ components/Cabecera.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-primary">
+            <nuxt-link to="/" class="navbar-brand">POKEMON API</nuxt-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Cabecera'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,72 @@
 <template>
-  <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        pinta-apis
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        <h1>Inicio</h1>
+        <b-container>
+            <b-row>
+                <b-col cols="4" v-for="(pokemon, iterator) in pokemons" :key="iterator">
+                    <b-card
+                        :title="pokemon.name"
+                        :img-src="'https://picsum.photos/id/' + (iterator + 50) + '/100/100'"
+
+                        img-alt="Imágen"
+                        style="max-with: 20rem;">
+                        <!--                        <p class="card-text">-->
+                        <!--                            tipo-->
+                        <!--                            {{ pokemon }}-->
+                        <!--                        </p>-->
+                        <b-button variant="primary" @click="getPokemonDetail(pokemon.url)">Ver mas...</b-button>
+                    </b-card>
+                </b-col>
+            </b-row>
+        </b-container>
+        <b-modal id="modal-1" title="HABILIDADES">
+            <div class="ability" v-if="pokemodal.abilities.length">
+                <p class="my-4" v-for="(ability, iterator) in pokemodal.abilities" :key="iterator">Habilidad:
+                    {{ ability.ability.name }}</p>
+            </div>
+        </b-modal>
     </div>
-  </div>
+
 </template>
 
 <script>
-export default {}
+
+import axios from 'axios'
+//axios.defaults.headers.common['user-key'] = '7y9uadbf8z83il0wu2bmct3dapum4u'
+axios.defaults.headers.common['x-Requested-With'] = 'XMLHttpRequest'
+
+export default {
+    data() {
+        return {
+            pokemons: [], //aqui guardas los pokemon que te vas a traer
+            pokemodal: {
+                abilities: []
+            },
+        }
+    },
+    async created() { //esto se hace cuando se crea la pagina
+        let res = await axios.get('https://pokeapi.co/api/v2/pokemon')
+        console.log('created')
+        console.log(res)
+    },
+    async asyncData() {  //aqui todo el tema del ajax con axios esto se hace antes de renderizarse la página
+        //const cors = 'https://cors-anywhere.herokuapp.com/corsdemo'
+        let res = await axios.get('https://pokeapi.co/api/v2/pokemon')
+        console.log(res)
+        return {
+            pokemons: res.data.results //guardo en pokemon todos los datos de la petición
+        }
+    },
+    methods: {
+        getPokemonDetail(url) {
+            axios.get(url).then(response => {
+                console.log(response.data)
+                this.pokemodal = response.data;
+                this.$bvModal.show('modal-1');
+            })
+
+        }
+    }
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
